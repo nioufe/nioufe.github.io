b91e8330b8149b6bfc8206ed0ab8a91af1986e6e
TrauDeRien <adrientn@gmail.com>
2014-06-30 14:31:33 +0200

    links fonctionnel

--- scripts/Startups.js
+++ scripts/Startups.js
@@ -25,9 +25,46 @@ var Startups = {};
 
         return rootNode;
     };
+    //TODO optimiser mais possible perte de clarte du code
     var keywords = function (nodes){
+        var words = [];
+        var links = [];
+        // recuperation des mots clefs
+        nodes.forEach(function(node){
+           if(node.keywords){
+               words = _.union(words, node.keywords);
+           }
+        });
 
+        // creation des liens
+        words.forEach(function(word){
+          // recuperation des node en rapport avec le mot
+          var toBind =  _.filter(nodes, function(node){
+             return _.contains(node.keywords, word);
+          });
+          // creation des liens
+          toBind.forEach(function(node1){
+              toBind.forEach(function(node2){
+                 if(node1 !== node2){
+                     var link = _.find(links, function(link){
+                        var isNodes = (link.source == node1 && link.target === node2)
+                         || (link.source == node2 && link.target == node1);
+                        return isNodes;
+                     })
+                     if(!link){
+                         links.push({source: node1, target: node2, keywords : [word]})
+                     }
+                     else if(!_.contains(link.keywords, word)){
+                         link.keywords.push(word);
+                     }
+                 }
+              });
+          });
+        });
+
+        return links;
     };
 
     Startups.root = root;
+    Startups.keywords = keywords;
 })();
