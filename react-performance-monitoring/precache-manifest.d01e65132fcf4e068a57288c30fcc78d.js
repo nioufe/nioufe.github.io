47a6bc8c49491bcd4800298c4ded070f56c8fc9a
Adrien Trauth <adrien.trauth@datadoghq.com>
2020-05-10 17:48:11 +0200

    add perf monitoring

---
+++ react-performance-monitoring/precache-manifest.d01e65132fcf4e068a57288c30fcc78d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998ecd0ad92d22c1190710e54ef88d98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b4bad6dd4e47afaf3a8",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "df66f2203e14b3660f6e",
+    "url": "/static/js/2.e7479b7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7479b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4bad6dd4e47afaf3a8",
+    "url": "/static/js/main.cacade6d.chunk.js"
+  },
+  {
+    "revision": "69cb81a785c0783c7499",
+    "url": "/static/js/runtime-main.e2c7c759.js"
+  }
+]);
